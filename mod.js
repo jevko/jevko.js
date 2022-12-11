48b939cd098f64a01716a5f8be1e9183f7df8fc3
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-11 02:44:32 +0100

    export default delimiters from mod

--- mod.js
+++ mod.js
@@ -1,3 +1,4 @@
 export {jevkoFromString} from './jevkoFromString.js'
 export {jevkoToString, stringToHeredoc} from './jevkoToString.js'
 export {escape} from './escape.js'
+export {defaultDelimiters} from './delimiters.js'
