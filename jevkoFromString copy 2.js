56575f18a200779b0d8b2a03ad1773b6eabaa155
Darius J Chuck <darius.j.chuck+github@gmail.com>
2023-11-30 21:14:53 +0100

    refactor a bit

--- jevkoFromString copy 2.js
+++ jevkoFromString copy 2.js
@@ -1,56 +1,56 @@
 import { normalizeDelimiters } from "./delimiters.js"
 
 // a version which does multistrings similarly to djevko
+// but in the most minimal way that does not know anything about whitespace
+// however it can easily be extended to support space and other djevko/djedat features
 export const jevkoFromString = (str, delimiters) => {
   const {opener, closer, escaper, quoter} = normalizeDelimiters(delimiters)
 
   const parents = []
   let parent = {subjevkos: []}, buffer = '', h = 0, mode = 'normal'
   let line = 1, column = 1
-  let tag = '', t = 0
+  let fence = '', t = 0
 
-  // todo: save tag + tag info in jevko
-  const open = (prefix, i, tag) => {
+  const open = (prefix, i) => {
     const jevko = {subjevkos: []}
-    parent.subjevkos.push({
-      prefix, 
-      ...(tag !== undefined && {tag}),
+    const subjevko ={
+      prefix,
       jevko,
-    })
+    }
+    parent.subjevkos.push(subjevko)
     buffer = ''
     h = i + 1
     parents.push(parent)
     parent = jevko
+    return subjevko
   }
 
-  // todo: save tag + tag info in jevko
-  const close = (suffix, i, tag) => {
+  const close = (suffix, i) => {
     parent.suffix = suffix
-    if (tag !== undefined) parent.tag = tag
     buffer = ''
     h = i + 1
     if (parents.length < 1) throw SyntaxError(`Unexpected closer (${closer}) at ${line}:${column}!`)
     parent = parents.pop()
   }
 
-  // note: checks tag from index 1, assuming tag[0] is always escaper
-  const parseQuoteMark = (i) => {
+  // note: checks fence from index 1, assuming fence[0] is always escaper
+  const parseFence = (i) => {
     // note: prepending escaper for symmetry
     // note: doubling escapers for symmetry
-    const tag = escaper + 
-          (buffer + str.slice(h, i))
-            .replaceAll(escaper, escaper + escaper)
+    const fence = escaper + 
+      (buffer + str.slice(h, i))
+        .replaceAll(escaper, escaper + escaper)
 
-    for (let i = 1; i < tag.length; ++i) {
-      const c = tag[i]
+    for (let i = 1; i < fence.length; ++i) {
+      const c = fence[i]
       if (c !== escaper) {
         throw SyntaxError(
-          `Quotation must start with ${escaper}${quoter} or ${escaper}${escaper}${escaper}${quoter} or ${escaper}${escaper}${escaper}${escaper}${escaper}${quoter} or... The number of ${escaper} before ${quoter} must be ODD! Instead found: ${tag.slice(1)}${escaper}${quoter}`
+          `Fenced text must start with ${escaper}${quoter} or ${escaper}${escaper}${escaper}${quoter} or ${escaper}${escaper}${escaper}${escaper}${escaper}${quoter} or... The number of ${escaper} before ${quoter} must be ODD! Instead found: ${fence.slice(1)}${escaper}${quoter}`
         )
       }
     }
 
-    return tag
+    return fence
   }
 
   // note: iterating thru code units rather than code points
@@ -62,26 +62,28 @@ export const jevkoFromString = (str, delimiters) => {
     if (mode === 'escaped') {
       if (c === escaper || c === opener || c === closer) mode = 'normal'
       else if (c === quoter) {
-        tag = parseQuoteMark(i)
-        mode = 'heredoc'
+        fence = parseFence(i)
+        mode = 'fenced'
         h = i + 1
         t = i + 1
       } else throw SyntaxError(`Invalid digraph (${escaper}${c}) at ${line}:${column}!`)
-    } else if (mode === 'heredoc') {
+    } else if (mode === 'fenced') {
       if (c === quoter) {
         t = i + 1
       } else if (c === opener) {
         const found = str.slice(t, i)
-        if (found === tag) {
-          // todo: save tag + tag info in jevko
-          open(str.slice(h, t - 1), i, tag)
+        if (found === fence) {
+          const sub = open(str.slice(h, t - 1), i)
+          // note: save fence + fence info in subjevko
+          sub.fence = fence
           mode = 'normal'
         } // else t = i + 1
       } else if (c === closer) {
         const found = str.slice(t, i)
-        if (found === tag) {
-          // todo: save tag + tag info in jevko
-          close(str.slice(h, t - 1), i, tag)
+        if (found === fence) {
+          // note: save fence + fence info in jevko
+          parent.fence = fence
+          close(str.slice(h, t - 1), i)
           mode = 'normal'
         } // else t = i + 1
       }
@@ -104,13 +106,13 @@ export const jevkoFromString = (str, delimiters) => {
   }
   // todo: better error msgs
   if (mode === 'escaped') throw SyntaxError(`Unexpected end after escaper (${escaper})!`)
-  else if (mode === 'heredoc') {
+  else if (mode === 'fenced') {
     const found = str.slice(t)
-    if (found === tag) {
+    if (found === fence) {
       parent.suffix = str.slice(h, t - 1)
-      parent.tag = tag
+      parent.fence = fence
     } else {
-      throw SyntaxError(`Unexpected end before heredoc closed! Expected tag: [${tag.slice(1)}], found: [${found}].`)
+      throw SyntaxError(`Unexpected end before fenced text closed!\nExpected: ${fence}\nFound: ${found}`)
     }
   }
   else /*if (mode === 'normal')*/ {
